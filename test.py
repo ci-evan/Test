dc456e5e11d11cd7e700e5ab492e0bcee92f6694
ci-evan <ci.evan.taylor@gmail.com>
2013-10-25 13:41:59 -0700

    Fixed formatting, added additional input prompts

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-# Test.py version 0.0
+# Test.py version 0.1
 # Evan Taylor
 
 def main():
-	name = raw_input("What is your name?")
-	print "Your name is " + name
+	name = raw_input("What is your name?\n> ")
+	quest = raw_input("What is your quest?\n> ")
+	color = raw_input("What is your favorite color?\n> ")
 
+	print "Your name is " + name + ", your quest is " + quest + ", and your favorite color is " + color +". Nice!"
 
 
 if __name__ == "__main__":
